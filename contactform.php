60ac0b3ee0888c52b79733963232119cf8dfb102
narwhaal <45606233+narwhaal@users.noreply.github.com>
2021-03-01 03:03:00 -0700

    Create contactform.php

---
+++ contactform.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Contact Us</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"
+    <link type="Text/css" rel="#" href="css/style.css">
+  </head>
+  
+  <body>
+    <div class="contact-us">
+      <form action="" method="post" class="popup-form">
+        <p>Contact Us</p><br>
+        <label for="Messages"><b>Messages</b></label><br>
+               <textarea placeholder="Your Messages" name="message" required></textarea><br>
+               <label for="email"><b>Email</b></label><br>
+               <input type="text" placeholder="Your email" name="email" required><br>
+               <button type="submit" class="button">Send</button>
+               <button type="button" class="button chatOff" onclick="closeForm()">Close</button>
+           </form>
+       </div>
+    
+    <script>
+           function openForm() {
+               document.getElementById("myForm").style.cssText = "display: block;";
+           }
+           function closeForm() {
+               document.getElementById("myForm").style.cssText = "display: none;";
+           }
+       </script>
+    
+  </body>
+  
+</html>
