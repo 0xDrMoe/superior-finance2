e98c9b63bda1a0a1e7e0e10082e5c037e2ffe802
narwhaal <45606233+narwhaal@users.noreply.github.com>
2021-02-28 23:40:53 -0700

    Create Contact-form.php

---
+++ Contact-form.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $subject = $_POST['subject'];
+  $emailFrom = $_POST['email'];
+  $message = $_POST['message'];
+  
+  $emailTo = "superiorfinanceinvest@gmail.com";
+  $headers = "From: ".$emailFrom;
+  $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+  
+  email($emailTo, $subject, $txt, $headers);
+  header("Location: index.php?mailsend");
+}
