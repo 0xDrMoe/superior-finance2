3c659ecb8ecbed80247f0b65661b8b3b51c59170
narwhaal <45606233+narwhaal@users.noreply.github.com>
2021-03-01 04:26:16 -0700

    Rename contactform.php to contactform2.php

--- contactform2.php
+++ contactform2.php
@@ -1,4 +1,35 @@
-<!DOCTYPE html>
+<?php
+  $message_sent = false;
+  if(isset($_POST['email']) && $_POST['email'] != ''){
+     
+    if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ){
+    
+    //submit the form 
+    $userName = $_POST['name'];
+    $userEmail = $_POST['email'];
+    $messageSubject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $to = "help@superiorfinance.io";
+    $body = "";
+
+    $body .="From: ".$userName. "\r\n";
+    $body .="Subject: ".$messageSubject. "\r\n";
+    $body .="Email: ".$userEmail. "\r\n";
+    $body .="Message: ".$message. "\r\n";
+
+    //mail($to, $messageSubject, $body);
+      
+    $message_sent = true;  
+    }
+    else{
+        $invalid_class_name = "form-invalid";
+    }
+  }
+    
+?>
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,6 +41,10 @@
 </head>
 
 <body>
+    <p>
+        Contact Us
+    </p>
+    
     <?php 
     if($message_sent):
     ?>
